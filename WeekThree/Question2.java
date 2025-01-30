c0f584c4cb0356af7c09a325580557cc6d7de87f
Ashmit kamboj <ashmitkamboj@gmail.com>
2025-01-30 20:16:35 +0530

    ques2

--- WeekThree/Question2.java
+++ WeekThree/Question2.java
@@ -16,5 +16,15 @@ public class Question2 {
             
             System.out.println();
         }
+
+        for (int i = n - 1; i >= 1; i--) 
+        {
+            for (int j = 1; j <= i; j++) 
+            {
+                System.out.print(j);     
+            }    
+
+            System.out.println();
+        }
     }
 }
